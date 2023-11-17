6d5726f8d9243e9d3585d1e5380eb0823af145c3
Eder Moreira de Oliveira <feelsgoback967@outlook.com>
2023-11-17 20:50:27 -0300

    ttcc 1.02.1
    Atualização da pagina de cadastro de produtos: add do BD e algumas validações

--- User/pages/cadastrar-produtos.php
+++ User/pages/cadastrar-produtos.php
@@ -1,6 +1,11 @@
 <?php 
     verificaPermissaoPagina(0);
 ?>
+<?php 
+    $sql = MySQL::conectar()->prepare("SELECT * FROM `tb_categoria`");
+    $sql->execute();
+    $categoria = $sql->fetchAll();
+?>
 <section class="box-content">
     <h2><i class="fa fa-pencil"></i> Adicione um produto: </h2>
     <form method="post" enctype="multipart/form-data">
@@ -16,6 +21,45 @@
                 $preco = Painel::formatarMoedaBd($_POST['preco']);
                 $imagens = array();
                 $amountFiles = count($_FILES['imagem']['name']);
+
+                $sucesso = true;
+                if ($_FILES['imagem']['name'][0] != '') {
+                    for ($i = 0; $i < $amountFiles; $i++) {
+                        $imagemAtual = [
+                            'type' => $_FILES['imagem']['type'][$i],
+                            'size' => $_FILES['imagem']['size'][$i]
+                        ];
+    
+                        if (Painel::imagemValida($imagemAtual) == false) {
+                            $sucesso = false;
+                            Painel::alert('erro', 'Uma das imagens selecionadas é invalida');
+                            break;
+                        }
+                    }
+                } else {
+                    $sucesso = false;
+                    Painel::alert('erro', 'Você precisa selecionar pelo menos uma imagem');
+                }
+
+                if($sucesso == true){
+                    for ($i = 0; $i < $amountFiles; $i++) {
+                        $imagemAtual = [
+                            'tmp_name' => $_FILES['imagem']['tmp_name'][$i],
+                            'name' => $_FILES['imagem']['name'][$i]
+                        ];
+                        $imagens[] = Painel::uploadFile($imagemAtual);
+                    }
+                    $sql = MySQL::conectar()->prepare("INSERT INTO `tb_produto.estoque` VALUES(null,?,?,?,?,?,?,?,?)");
+                    $sql->execute(array($nome,$descricao,$largura,$altura,$comprimento,$peso,$quantidade,$preco));
+                    $lastId = MySQL::conectar()->LastInsertId();
+                    foreach($imagens as $key => $value){
+                        $updateImagens = MySQL::conectar()->prepare("INSERT INTO `tb_produto.estoque_imagens` VALUES (null,?,?)");
+                        $updateImagens->execute([$lastId, $value]);
+                    }
+                    Painel::alert('sucesso','Produto cadastrado com sucesso');
+                }else{
+                    Painel::alert('erro', 'Não foi possivel cadastrar este produto. <br>Por favor tente novamente');
+                }
             }
         ?>
         <div class="form-group">
@@ -25,6 +69,14 @@
         <div class="form-group">
             <label for="">Descrição do Produto</label>
             <textarea name="descricao" style="resize: vertical;" placeholder="Camiseta preta manga longa slim"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="">Categoria:</label>
+            <select name="categorias">
+                <?php foreach($categoria as $key => $value){?>
+                    <option value=""><?php echo $value['nome']?></option>
+                <?php } ?>
+            <select>
         </div>
         <div class="form-group">
             <label for="">Largura do produto:</label>
